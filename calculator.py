6361daf0432ce6a94eb6eddf4ad83e1486f73ec1
calvin67-bht <calvinpappy@gmail.com>
2025-07-24 04:14:49 +0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return x + y
+def subtract(x,y):
+    return x - y
+def multiply(x,y):
+    return x* y
+def divide(x,y):
+    return x % y
+print(add(2,4))
+print(subtract(2,4))
+print(multiply(2,4))
+print(divide(2,4))
+
