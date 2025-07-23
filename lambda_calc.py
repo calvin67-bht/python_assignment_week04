eb854a460cf2aa881b4ab4c66d8e9b90ea319eed
calvin67-bht <calvinpappy@gmail.com>
2025-07-24 04:20:25 +0700

    create lamda_calc.py

---
+++ lambda_calc.py
@@ -0,0 +1,8 @@
+add = lambda x, y: x + y
+subtract = lambda x, y: x - y
+multiply = lambda x, y: x * y
+divide  = lambda x, y: x %  y
+print(add(2,4))
+print(subtract(2,4))
+print(multiply(2,4))
+print(subtract(2,4))
