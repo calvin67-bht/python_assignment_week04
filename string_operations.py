ccad4e27a6779f878912756a3f1a728153ccd896
calvin67-bht <calvinpappy@gmail.com>
2025-07-29 12:18:45 +0700

    update string_operations.py

---
+++ string_operations.py
@@ -0,0 +1,20 @@
+
+
+def reverse_string(s):
+    """Reverses the given string."""
+    return s[::-1]
+
+def capitalize_string(s):
+    """Capitalizes the first letter of the string."""
+    return s.capitalize()
+
+def lowercase_string(s):
+    """Converts all characters to lowercase."""
+    return s.lower()
+
+def uppercase_string(s):
+    """Converts all characters to uppercase."""
+    return s.upper()
+
+
+      
